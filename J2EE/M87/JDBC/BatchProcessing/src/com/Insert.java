df79711eedc41d5e7c5a5bf47b008fa5e71ee7e5
elangobeast <elangonith1107@gmail.com>
2025-04-07 14:33:39 +0530

    7-04-25 updated

--- J2EE/M87/JDBC/BatchProcessing/src/com/Insert.java
+++ J2EE/M87/JDBC/BatchProcessing/src/com/Insert.java
@@ -20,9 +20,9 @@ public class Insert {
 			
 			Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/movie_db","root","root");
 			
-			PreparedStatement  ps = c.prepareStatement("inser into movie values(?,?,?,?,?,?,?) ");
+			PreparedStatement  ps = c.prepareStatement("insert into movie values(?,?,?,?,?,?,?) ");
 					
-			for(int i=1; i<count ; i++) {
+			for(int i=1; i<=count ; i++) {
 				System.out.println("enter mid");
 				int mid = s.nextInt();
 				ps.setInt(1, mid);
@@ -38,21 +38,26 @@ public class Insert {
 				ps.setString(3, language);
 				
 				System.out.println("Enter hero");
-				String hero = s.nextLine();
+				String hero = s.next();
 			    ps.setString(4, hero);
 			    
 			    System.out.println("Enter heroine");
-			    String heroine = s.nextLine();
+			    String heroine = s.next();
 			    ps.setString(5, heroine);
 			    
 			    System.out.println("Enter Director");
-			    String director = s.nextLine();
+			    String director = s.next();
 			    ps.setString(6, director);
 			    
 			    System.out.println("Enter collection");
 			    double collection = s.nextDouble();
 			    ps.setDouble(7, collection);
+			    
+			    ps.addBatch();
 			}
+			
+			ps.executeUpdate();
+			System.out.println("Data inserted");
 			
 		} catch (ClassNotFoundException | SQLException e) {
 			
